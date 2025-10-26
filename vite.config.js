7d050c33f2702331903615d82fd1cfb1afabe30f
Brayan Presiga <presigabrayan3@gmai.com>
2025-10-25 21:33:32 -0500

    Update page's name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/bingo-traditional-don-sepu-control-pane/",
+  base: "/bingo-traditional-don-sepu-control-panel/",
 });
